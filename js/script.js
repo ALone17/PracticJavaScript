f64203e5435c31c281d138c70599e2d00fef3013
Anton <tosha.kondratenko@yandex.ru>
2021-05-24 18:00:37 +0300

    добавил комменты'

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?',"");
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?',""); // создание переменной которой присваивается значение через функцию Prompt
 
-const personalMovieDB = {
+const personalMovieDB = {   //создание объекта
     count: numberOfFilms,
-    movies: {},
+    movies: {}, // внутри объекта можно создать ключ, у которого значение так же может быть объектом
     actors: {},
     genres: {},
     private: false
 };
 
+// создание переменных и присваивание им значений через функцию Prompt
 const a= prompt('Один из последних просмотренных фильмов', ''),
       b= prompt('На сколько вы оцените его', ''),
       c= prompt('Один из последних просмотренных фильмов', ''),
       d= prompt('На сколько вы оцените его', '');
-
-    personalMovieDB.movies[a] = b;
+// присваивание новых объектов ключу movies
+    personalMovieDB.movies[a] = b; 
     personalMovieDB.movies[c] = d;
     
     console.log(personalMovieDB);
