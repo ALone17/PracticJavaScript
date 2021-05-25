cb1f1b20c531fa1322ee0fd1a8019ff255d7f1e3
Anton <tosha.kondratenko@yandex.ru>
2021-05-25 15:40:11 +0300

    добавлен цикл и условия

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?',""); // создание переменной которой присваивается значение через функцию Prompt
 
+
 const personalMovieDB = {   //создание объекта
     count: numberOfFilms,
     movies: {}, // внутри объекта можно создать ключ, у которого значение так же может быть объектом
@@ -11,12 +12,37 @@ const personalMovieDB = {   //создание объекта
 };
 
 // создание переменных и присваивание им значений через функцию Prompt
-const a= prompt('Один из последних просмотренных фильмов', ''),
-      b= prompt('На сколько вы оцените его', ''),
-      c= prompt('Один из последних просмотренных фильмов', ''),
-      d= prompt('На сколько вы оцените его', '');
-// присваивание новых объектов ключу movies
-    personalMovieDB.movies[a] = b; 
-    personalMovieDB.movies[c] = d;
+
+// присваивание через цикл новых объектов ключу movies
+   for(let i = 0; i < 2; i++){
+    let a= prompt('Один из последних просмотренных фильмов', ''),
+        b= prompt('На сколько вы оцените его', '');
+        if(a === null || b === null){
+            i--;
+        }
+        else if(a.length > 50 || a.length == 0){ 
+            i--;
+        }
+        else if( b.length > 50 || b.length == 0 ){
+            i--;
+        }
+        else{
+            personalMovieDB.movies[a]=b;
+        }
+        
+   }
+
+   // практика с условиями
+if(personalMovieDB.count < 10){
+    console.log("Просмотренно довольно мало фильмов");
+}
+else if(personalMovieDB.count <= 30){
+    console.log("Вы классический зритель");
+}
+else{
+    console.log("Вы киноман");
+}
     
     console.log(personalMovieDB);
+
+  
